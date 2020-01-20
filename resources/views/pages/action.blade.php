8f9ecd8a61aaecf133619675c6f2eba0809be392
ITlysa <lysa.thorn@gmail.com>
2020-01-20 09:17:01 +0700

    @yield @section @include @extends

--- resources/views/pages/action.blade.php
+++ resources/views/pages/action.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{$action}}</title>
-</head>
-<body>
-    <h1>Action</h1>
+
+   @extends('template')
+   @section('content')
+       This is Action.
+   @endsection
     
-</body>
-</html>
